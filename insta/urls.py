d91a5063e804da5a764bd609dcac149d70c89286
danalvin <mungaiwaituika@gmail.com>
2018-10-05 07:13:12 +0300

    ROutes adjusted

--- insta/urls.py
+++ insta/urls.py
@@ -21,7 +21,7 @@ from django.contrib.auth import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url('/',include('Gram.urls')),
+    url(r'',include('Gram.urls')),
     url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^logout/$', views.logout, {"next_page": '/'}), 
 
